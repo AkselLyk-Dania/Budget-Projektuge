8d377a772adb144e9cb2c2a64b9e6cdea86c8509
NicolasHO-eadania <niov6@student.eadania.dk>
2025-10-30 12:07:05 +0100

    Alt virker

--- Budget/Budget/MainWindow.xaml.cs
+++ Budget/Budget/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -61,7 +62,7 @@ namespace Budget
             InputNavn.Clear();
             InputKroner.Clear();
 
-            ListBoxIndtaegter.Items.Add(stringInputNavn + " " + stringInputKroner);
+            ListBoxIndtaegter.Items.Add(stringInputNavn + ": +" + stringInputKroner + "kr");
         }
 
         private void TilføjTilUdgifter(object sender, RoutedEventArgs e)
@@ -76,12 +77,20 @@ namespace Budget
             InputNavn.Clear();
             InputKroner.Clear();
 
-            ListBoxUdgifter.Items.Add(stringInputNavn + " " + stringInputKroner);
+            ListBoxUdgifter.Items.Add(stringInputNavn + ": -" + stringInputKroner + "kr");
         }
 
         private void Udregn(object sender, RoutedEventArgs e)
         {
-          
+            int indtaegterSum = indtægter.liste.Sum(Item => Item.kroner);
+            int udgifterSum = udgifter.liste.Sum(Item => Item.kroner);
+
+            int sumAfBegge = indtaegterSum - udgifterSum;
+            string sumText = Convert.ToString(sumAfBegge);
+
+            SumBox.Clear();
+
+            SumBox.Text = sumText + "kr";
         }
 
         public void KronerInput(object sender, TextCompositionEventArgs e)
@@ -103,6 +112,40 @@ namespace Budget
         private void SelectionChanged_Udgifter(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void SumBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void ButtonReset_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Reset", "Er du sikker på at du vil starte forfra?", MessageBoxButton.OKCancel);
+            ListBoxIndtaegter.Items.Clear();
+            ListBoxUdgifter.Items.Clear();
+            SumBox.Clear();
+
+            indtægter.liste.Clear();
+            udgifter.liste.Clear();
+        }
+
+        private void ButtonUndoIndtaegter_Click(object sender, RoutedEventArgs e)
+        {
+            if(indtægter.liste.Count > 0)
+            {
+                indtægter.liste.RemoveAt(indtægter.liste.Count - 1);
+                ListBoxIndtaegter.Items.RemoveAt(ListBoxIndtaegter.Items.Count - 1);
+            }
+        }
+
+        private void ButtonUndoUdgifter_Click(object sender, RoutedEventArgs e)
+        {
+            if(udgifter.liste.Count > 0)
+            {
+                udgifter.liste.RemoveAt(udgifter.liste.Count - 1);
+                ListBoxUdgifter.Items.RemoveAt(ListBoxUdgifter.Items.Count - 1);
+            }
         }
     }
 }
